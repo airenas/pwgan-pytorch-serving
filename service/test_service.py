241058db3cb3843991fa1f7b7cff9d5e9b2e3cd5
Airenas Vaičiūnas <airenass@gmail.com>
2020-11-09 12:59:09 +0200

    Add service tests

---
+++ service/test_service.py
@@ -0,0 +1,79 @@
+import os
+
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+
+from service import service
+
+
+def init_test_app():
+    app = FastAPI()
+    service.setup_requests(app)
+    service.setup_routes(app)
+    service.setup_vars(app)
+    client = TestClient(app)
+    return client, app
+
+
+def test_read_main():
+    client, _ = init_test_app()
+
+    response = client.get("/")
+    assert response.status_code == 404
+
+
+def test_info():
+    os.environ["MODEL_NAME"] = "m1"
+    client, app = init_test_app()
+
+    response = client.get("/info")
+    assert response.status_code == 200
+    assert response.json() == {'loaded': False, 'name': 'm1'}
+
+    app.model_loaded = True
+    response = client.get("/info")
+    assert response.status_code == 200
+    assert response.json() == {'loaded': True, 'name': 'm1'}
+
+
+def test_calculate_fail():
+    client, _ = init_test_app()
+
+    response = client.get("/model")
+    assert response.status_code == 405
+
+    response = client.post("/model", json={"olia": "olia"})
+    assert response.status_code == 422
+
+    response = client.post("/model", json={"olia": "olia"})
+    assert response.status_code == 422
+
+
+def test_calculate_fail_empty():
+    client, _ = init_test_app()
+
+    response = client.post("/model", json={"data": ""})
+    assert response.status_code == 400
+
+
+def test_calculate():
+    client, app = init_test_app()
+
+    def test_calc(text, model):
+        assert text == "in text"
+        assert model == "m"
+        return "olia"
+
+    app.calculate = test_calc
+    response = client.post("/model", json={"data": "in text", "model": "m"})
+    assert response.status_code == 200
+    assert response.json() == {"data": "olia", "error": None}
+
+
+def test_environment():
+    os.environ["MODEL_NAME"] = "m1"
+    os.environ["MODEL_PATH"] = "/m"
+    ta = FastAPI()
+    service.setup_vars(ta)
+    assert ta.model_name == "m1"
+    assert ta.model_path == "/m"
