e77c208dcd7bd152908d8f08f903ef2b57433822
Airenas Vaičiūnas <airenass@gmail.com>
2021-03-23 15:40:33 +0200

    Add date to logs

--- run.py
+++ run.py
@@ -3,13 +3,20 @@ import sys
 
 import uvicorn
 from service.service import create_service
+from uvicorn.config import LOGGING_CONFIG
 
 from fastapi.logger import logger
 import logging
 
 ll = os.environ.get('LOG_LEVEL', 'INFO').upper()
 logger.setLevel(level=ll)
-logger.handlers = [logging.StreamHandler(sys.stdout)]
+handler = logging.StreamHandler(sys.stdout)
+logger.handlers = [handler]
+formatter = logging.Formatter("[%(asctime)s.%(msecs)03d] %(levelname)s - %(message)s", "%Y-%m-%d %H:%M:%S")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+LOGGING_CONFIG["formatters"]["default"]["fmt"] = "[%(asctime)s.%(msecs)03d] %(levelname)s - %(message)s"
+LOGGING_CONFIG["formatters"]["default"]["datefmt"] = "%Y-%m-%d %H:%M:%S"
 
 logger.info("Starting pwgan-pytorch-serving service")
 app = create_service()
