34f94cd654fdcfc997227e4506ec497f194f8f0f
Airenas Vaičiūnas <airenass@gmail.com>
2021-09-13 18:18:51 +0300

    Add time tracking test

--- service/test_metrics.py
+++ service/test_metrics.py
@@ -1,6 +1,8 @@
+import time
+
 from prometheus_client import REGISTRY
 
-from service.metrics import MetricsKeeper
+from service.metrics import MetricsKeeper, ElapsedLogger
 
 
 def test_metrics():
@@ -9,3 +11,11 @@ def test_metrics():
     metrics.load_metric.observe(2)
     assert REGISTRY.get_sample_value("pwgan_model_calc_seconds_sum") == 1
     assert REGISTRY.get_sample_value("pwgan_model_load_seconds_sum") == 2
+
+
+def test_elapsed_logger():
+    def func(msg: str, *args, **kwargs):
+        assert msg.startswith("test: 0.1")
+
+    with ElapsedLogger(func, "test"):
+        time.sleep(0.1)
